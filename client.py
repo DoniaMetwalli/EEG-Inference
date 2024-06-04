8f612136138ca1098cef24926c6b992982fc7395
Antonykiroles601 <antony_esk@hotmail.com>
2024-06-04 16:24:15 +0300

    Inference complete with headset connected to server on linux and client on windows

--- client.py
+++ client.py
@@ -1,12 +1,9 @@
 import asyncio
-import aiofiles
+import threading
 import websockets
-from fastapi import WebSocket
 from ahk import AHK
-from PythonWrapper import Unicorn
-from dataclasses import dataclass
-import numpy as np
 
+import numpy as np
 ahk = AHK()
 
 def rightClass():
@@ -25,110 +22,40 @@ def selectClass():
     ahk.run_script("Run https://github.com/")
     
 
-@dataclass
-class UnicornConfig:
-    """
-    Config For Experiment Instance
-    RecordLength and BreakLength are in milliseconds
-    """
-    HeadsetConfig: Unicorn.UnicornAmplifierConfiguration = None
-    HeadsetSerial: str = "UN-2021.12.19"
-
-
-
-class UnicornHeadset:
-    def __init__(self, config: UnicornConfig):
-        self.config = config
-        self.OutputQueue: asyncio.Queue[list[float]] = asyncio.Queue()
-
-    def RecordContinuously(self, run = None):
-        if self.config.HeadsetConfig is None:
-            raise Exception("Headset Config Not Set")
-        while True :
-            getDataOutput = self.Unicorn.GetData(
-                self.HandleVal, 1, len(self.config.HeadsetConfig.channels)
-            )
-            if getDataOutput[1] == Unicorn.UnicornReturnStatus.Success:
-                # print("GOT DATA")
-                # print(getDataOutput)
-                self.OutputQueue.put(np.array(getDataOutput[0], dtype=np.float32))
-            else:
-                print("Failed to get Data", getDataOutput[1])
-
-    def Config(self):
-            self.Unicorn = Unicorn
-            try:
-                OpenDeviceOut = self.Unicorn.OpenDevice(self.config.HeadsetSerial)
-                if OpenDeviceOut[2] != Unicorn.UnicornReturnStatus.Success:
-                    raise Exception("Device Not Found")
-                self.HandleRef = OpenDeviceOut[0]
-                self.HandleVal = OpenDeviceOut[1]
-
-                CurrentConfig = self.Unicorn.GetConfiguration(self.HandleVal)
-                print("Current Config: ", CurrentConfig[1])
-                if self.config.HeadsetConfig is not None:
-                    setConfigOut = self.Unicorn.SetConfiguration(
-                        self.HandleVal, self.config.HeadsetConfig
-                    )
-                    if setConfigOut != Unicorn.UnicornReturnStatus.Success:
-                        raise Exception("Failed to set Configuration")
-                else:
-                    self.config.HeadsetConfig = CurrentConfig[0]
-            except Exception as e:
-                print(e)
-
-## not done yet, a bit unclear, will do it in uni
-async def headsetSignals(websocket: WebSocket):
-    signal = UnicornHeadset()
-    signal.config()
-    return signal.OutputQueue    
-
-async def reading(websocket: WebSocket, queue):
-    async with aiofiles.open('Session7.csv', mode='r') as f:
-        async for line in f:
-            await websocket.send(line)
-                
-async def sending(websocket: WebSocket):
+async def recvThread(websocket: websockets.WebSocketClientProtocol):
     while True:
-        greeting = await websocket.recv()
-        print(greeting)
-        
+        classification = await websocket.recv()
+        yield classification
+
+async def sendThread(websocket: websockets.WebSocketClientProtocol):
+    while True:
+        await websocket.send("Hello")
+        await asyncio.sleep(0.1)
+
+
 async def all():
     async with websockets.connect("ws://localhost:8090/ws") as websocket:
 
-        queue = asyncio.Queue()
 
-        sending_task = asyncio.create_task(sending(websocket))
-        reading_task = asyncio.create_task(reading(websocket))
-        
-        await queue.put(sending_task)
-        await queue.put(reading_task)
-        
-        sendingQ = await queue.get()
-        await sendingQ
-        queue.task_done()
-        
-        readingQ = await queue.get()
-        await readingQ
-        queue.task_done()
-        
-        await queue.join()
-        
-        
-        ## will need optimization for sure bas di example 
-        classification = await websocket.recv()
-        
-        ## check classification spelling (case sensitive)
-        if (classification == "right"): 
-            rightClass()
-        elif (classification == "left"):
-            leftClass()
-        elif (classification == "up"):
-            upClass()
-        elif (classification == "down"):
-            downClass()
-        else: ## classification == "select"
-            selectClass()
+        sending_task = asyncio.create_task(sendThread(websocket))
+
+        async for classification in recvThread(websocket):
+            ## check classification spelling (case sensitive)
+            if (classification == "Right"): 
+                rightClass()
+            elif (classification == "Left"):
+                leftClass()
+            elif (classification == "Up"):
+                upClass()
+            elif (classification == "Down"):
+                downClass()
+            elif classification == "Select":
+                selectClass()
+            else:
+                print(classification)
+                print("Invalid Classification")
+        await sending_task
+
 
     
 asyncio.run(all())
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 
+import threading
 from fastapi import FastAPI, WebSocket
 from fastapi.websockets import WebSocketState
 import numpy as np
 import asyncio
 from Inference import load_models, Inference
 from dataclasses import dataclass
+from PythonWrapper import Unicorn
+from queue import Queue, Empty
 
 @dataclass(slots=True)
 class SharedList:
@@ -14,10 +17,78 @@ app = FastAPI()
 
 
 
+@dataclass
+class UnicornConfig:
+    """
+    Config For Experiment Instance
+    RecordLength and BreakLength are in milliseconds
+    """
+    HeadsetConfig: Unicorn.UnicornAmplifierConfiguration = None
+    HeadsetSerial: str = "UN-2021.12.19"
+
+
+
+class UnicornHeadset:
+    def __init__(self, config: UnicornConfig):
+        self.config = config
+        self.OutputQueue: Queue[np.ndarray] = Queue()
+
+    def RecordContinuously(self):
+        if self.config.HeadsetConfig is None:
+            raise Exception("Headset Config Not Set")
+    
+        self.Unicorn.StartAcquisition(self.HandleVal, False)
+        while True :
+            getDataOutput = self.Unicorn.GetData(
+                self.HandleVal, 1, len(self.config.HeadsetConfig.channels)
+            )
+            if getDataOutput[1] == Unicorn.UnicornReturnStatus.Success:
+                # print("GOT DATA")
+                # print(getDataOutput)
+                self.OutputQueue.put(np.array(getDataOutput[0], dtype=np.float32))
+            else:
+                print("Failed to get Data", getDataOutput[1])
+
+    def Config(self):
+            self.Unicorn = Unicorn
+            try:
+                OpenDeviceOut = self.Unicorn.OpenDevice(self.config.HeadsetSerial)
+                print("FNOIFNIOEFN")
+                if OpenDeviceOut[2] != Unicorn.UnicornReturnStatus.Success:
+                    raise Exception("Device Not Found")
+                print("DWNFNIWOFf")
+                self.HandleRef = OpenDeviceOut[0]
+                self.HandleVal = OpenDeviceOut[1]
+
+                CurrentConfig = self.Unicorn.GetConfiguration(self.HandleVal)
+                print("Current Config: ", CurrentConfig[1])
+                if self.config.HeadsetConfig is not None:
+                    setConfigOut = self.Unicorn.SetConfiguration(
+                        self.HandleVal, self.config.HeadsetConfig
+                    )
+                    if setConfigOut != Unicorn.UnicornReturnStatus.Success:
+                        raise Exception("Failed to set Configuration")
+                else:
+                    self.config.HeadsetConfig = CurrentConfig[0]
+            except Exception as e:
+                print(e)
+
+## not done yet, a bit unclear, will do it in uni
+def headsetSignals():
+    hc = UnicornConfig()
+    signal = UnicornHeadset(hc)
+    signal.Config()
+    print("CONFIG")
+    return signal
+
+
+
 async def receiveThread(clientSocket:WebSocket, inputQueue:SharedList):
     try:
         while clientSocket.client_state == WebSocketState.CONNECTED:
-            inputQueue.sl.append(np.frombuffer(await clientSocket.receive_bytes(),dtype=np.float32).reshape(1,8,1))
+            # inputQueue.sl.append(np.frombuffer(await clientSocket.receive_bytes(),dtype=np.float32).reshape(1,8,1))
+            await asyncio.sleep(0.010)
+            await clientSocket.receive_text()
     except Exception as e:
         print(e)
         try:
@@ -25,10 +96,20 @@ async def receiveThread(clientSocket:WebSocket, inputQueue:SharedList):
         except Exception:
             pass
 
-async def inferenceThread(clientSocket:WebSocket,inputQueue:SharedList, batchSize: int = 1200):
+async def inferenceThread(clientSocket:WebSocket,inputQueue:SharedList, HeadsetQueue:Queue[np.ndarray], batchSize: int = 1200):
     loaded_xgb, scaler, label_encoder, loaded_autoencoder = load_models()
     try:
+
         while clientSocket.client_state == WebSocketState.CONNECTED:
+            
+            try:
+                signal = HeadsetQueue.get_nowait()
+                print(signal.shape)
+                inputQueue.sl.append(signal[:8].reshape(1,8,1))
+                print(inputQueue.sl[-1].shape)
+            except Empty:
+                await asyncio.sleep(0.001)
+
             if len(inputQueue.sl) >= batchSize:
                 batch = np.concatenate(inputQueue.sl[:batchSize],axis=2)
                 inputQueue.sl=inputQueue.sl[batchSize:]
@@ -44,7 +125,10 @@ async def inferenceThread(clientSocket:WebSocket,inputQueue:SharedList, batchSiz
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     inputQueue = SharedList(sl=[])
+    headset = headsetSignals()
+    recordThread = threading.Thread(target=headset.RecordContinuously)
+    recordThread.start()
     recvTask = asyncio.create_task(receiveThread(websocket,inputQueue))
-    async for out in inferenceThread(websocket,inputQueue):
+    async for out in inferenceThread(websocket,inputQueue, headset.OutputQueue):
         await websocket.send_text(out)
     await recvTask
